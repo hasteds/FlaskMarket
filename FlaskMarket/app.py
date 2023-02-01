20edb945ca6cfad30c4f99763a1935544b0295f9
hasteds <tin.petr@gmail.com>
2023-02-01 22:54:53 +0100

    added about and market.htm

--- FlaskMarket/app.py
+++ FlaskMarket/app.py
@@ -3,10 +3,20 @@ from flask import Flask, render_template, redirect, request
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def home():
-    return "<h1>Hello Tin and Daria</h1>"
+    return render_template("home.html")
 
 
 @app.route("/about/<username>")
 def about(username):
-    return f"<h1>This is about page of {username} </h1>"
+    return render_template("about.html")
+
+@app.route("/market")
+def market_page():
+    items = [
+    {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
+    {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
+    {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
+    ]
+    return render_template("market.html", items=items)
