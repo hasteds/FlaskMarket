52749659eb5809cda4c5a9fabec7685b6351a84d
hasteds <tin.petr@gmail.com>
2023-02-01 11:04:01 +0100

    new

--- FlaskMarket/app.py
+++ FlaskMarket/app.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello World"
+    return "<h1>Hello Tin and Daria</h1>"
+
+
+@app.route("/about/<username>")
+def about(username):
+    return f"<h1>This is about page of {username} </h1>"
