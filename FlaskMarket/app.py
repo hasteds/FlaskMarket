459e65489a3df7b8f2a3a554ebd6ca1cb14cf570
hasteds <tin.petr@gmail.com>
2023-02-03 09:48:16 +0100

    new features

--- FlaskMarket/app.py
+++ FlaskMarket/app.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template, redirect, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
+with app.app_context():
+    db = SQLAlchemy(app)
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=20), nullable=False, unique=True)
+    description = db.Column(db.String(length=2000), nullable=False, unique=True)
+    
+    def __repr__(self):
+        return f"Item {self.name}"
 
 @app.route("/")
 @app.route("/home")
@@ -12,11 +27,8 @@ def home():
 def about(username):
     return render_template("about.html")
 
+
 @app.route("/market")
 def market_page():
-    items = [
-    {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-    {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
-    {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
-    ]
+    items = Item.query.all()
     return render_template("market.html", items=items)
