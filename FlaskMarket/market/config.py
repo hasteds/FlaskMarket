0117e908f4261e28ddd40474c3ac0d25f5bd101a
hasteds <tin.petr@gmail.com>
2023-02-05 13:37:11 +0100

    Implemented register

--- FlaskMarket/market/config.py
+++ FlaskMarket/market/config.py
@@ -4,3 +4,4 @@ class Config(object):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = "sqlite:///market.db"
+    SECRET_KEY = "0341febe13e7b1011ad6ed17"
--- FlaskMarket/market/routes.py
+++ FlaskMarket/market/routes.py
@@ -1,10 +1,11 @@
 from market import app
 from flask import render_template
 from market.models import Item, User
+from market.forms import RegisterForm
 
 @app.route("/")
 @app.route("/home")
-def home():
+def home_page():
     return render_template("home.html")
 
 
@@ -12,3 +13,9 @@ def home():
 def market_page():
     items = Item.query.all()
     return render_template("market.html", items=items)
+
+
+@app.route("/register")
+def register_page():
+    form = RegisterForm()
+    return render_template("register.html", form=form)
